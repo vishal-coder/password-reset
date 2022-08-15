d786945f45f7149e01715f345e6e451d5f011f7e
vishal Coder <[vishal2app@gmail.com]>
2022-08-15 15:20:20 +0530

    added portfoliomail

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 import { authRouter } from "./routes/auth.js";
 import { MongoClient } from "mongodb";
 import cors from "cors";
+import { portfolioMailRouter } from "./routes/apiRouter.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/auth", authRouter);
+app.use("/api", portfolioMailRouter);
 
 dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
---
+++ routes/apiRouter.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+
+dotenv.config();
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.send("API route working");
+});
+
+router.post("/contactMail", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    auth: {
+      user: process.env.SYSTEM_EMAIL_ID,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    to: email,
+    from: process.env.SYSTEM_EMAIL_ID,
+    subject: "Portfolio site contact mail",
+    html: `<h1>New Contact from Portfolio site</h1>
+          <h2>User name : ${name}</h2>
+          <h2>User name : ${email}</h2>
+          <h2>User name : ${subject}</h2>
+          <h2>User name : ${message}</h2>
+          </div>`,
+  };
+
+  return transporter.sendMail(mailOptions);
+});
+
+export const portfolioMailRouter = router;
